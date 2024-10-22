42d5c154769180e7eb6a036f2ce067aaac7ce074
tnhunter <tnhunter2004@gmail.com>
2024-10-22 01:54:47 -0400

    TEMP : progress on csv

--- src/test/manualTest/ca/mrt/gs_backend/RocksDBUtils/testRocksDbToCsv.java
+++ src/test/manualTest/ca/mrt/gs_backend/RocksDBUtils/testRocksDbToCsv.java
@@ -3,8 +3,6 @@ package manualTest.ca.mrt.gs_backend.RocksDBUtils;
 import ca.mrt.gs_backend.RocksDBUtils.DataPacket;
 import ca.mrt.gs_backend.RocksDBUtils.MdbToCsv;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 import java.util.List;
 
 public class testRocksDbToCsv {
@@ -13,7 +11,6 @@ public class testRocksDbToCsv {
     @Test
     public void testGetBytes() {
         MdbToCsv converter = new MdbToCsv();
-        converter.setColumnHeaders(Arrays.asList("_metadata_","rt_data","parameter_archive"));
         try {
             List<DataPacket> packets = converter.getPackets();
             converter.getData(packets);
@@ -21,6 +18,8 @@ public class testRocksDbToCsv {
         }catch (Exception e) {
             e.printStackTrace();
         }
+
+        converter.writeToCsv("AAAAAAAAAAAAAAAAAAA.csv");
 
 
 
