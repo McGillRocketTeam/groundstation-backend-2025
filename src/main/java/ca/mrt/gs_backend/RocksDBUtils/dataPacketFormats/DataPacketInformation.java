2702d19559b1e1e62d1130a706fe567485b53397
tnhunter <tnhunter2004@gmail.com>
2024-10-21 22:04:09 -0400

    Successfully parsed into java class

--- src/main/java/ca/mrt/gs_backend/RocksDBUtils/dataPacketFormats/DataPacketInformation.java
+++ src/main/java/ca/mrt/gs_backend/RocksDBUtils/dataPacketFormats/DataPacketInformation.java
@@ -1,5 +1,17 @@
 package ca.mrt.gs_backend.RocksDBUtils.dataPacketFormats;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.LinkedTreeMap;
+
 public interface DataPacketInformation {
     public String getInformationAsCSV();
+    public void getFromJSONArray(JsonArray jsonArray);
+
+
+    default float getFloatFromJson(JsonElement element) {
+        JsonElement map =(element.getAsJsonObject().get("engValue"));
+        return ((JsonObject) map).get("floatValue").getAsFloat();
+    }
 }
