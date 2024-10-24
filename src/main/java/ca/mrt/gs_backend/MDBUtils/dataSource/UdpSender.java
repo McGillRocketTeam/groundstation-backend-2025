ea2454e8b3aa4bd620ea035a013e7685f37f75dc
tnhunter <tnhunter2004@gmail.com>
2024-10-24 15:38:21 -0400

    Code cleanup + now parses more than 100 packets without infinite loop!

--- src/main/java/ca/mrt/gs_backend/MDBUtils/dataSource/UdpSender.java
+++ src/main/java/ca/mrt/gs_backend/MDBUtils/dataSource/UdpSender.java
@@ -1,4 +1,4 @@
-package manualTest.ca.mrt.gs_backend.RocksDBUtils;
+package ca.mrt.gs_backend.MDBUtils.dataSource;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -8,13 +8,10 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UdpSender {
-    public static void main(String[] args) {
-        // Configuration
-        int shmolSize = 59;
-        String host = "localhost";
-        int port = 10035;
-        String filePath = "src/test/manualTest/ca/mrt/gs_backend/RocksDBUtils/files/packets - Copy.raw";
 
+    public static void sendPackets(int port, String filePath) {
+        int packetSize = 59; //size of a packet 59 for Labjack
+        String host = "localhost";
 
         try (DatagramSocket socket = new DatagramSocket()) {
             // Read the raw file
@@ -24,21 +21,21 @@ public class UdpSender {
                 fis.read(data);
             }
 
-
             int offset = 0;
             int total_size = 0;
             while (offset < data.length) {
-                int packetSize = Math.min(shmolSize, data.length - offset);
-                DatagramPacket packet = new DatagramPacket(data, offset, packetSize, InetAddress.getByName(host), port);
+                int size = Math.min(packetSize, data.length - offset);
+                DatagramPacket packet = new DatagramPacket(data, offset, size, InetAddress.getByName(host), port);
                 socket.send(packet);
-                System.out.println("Sent packet of size " + packetSize + " bytes to " + host + ":" + port);
-                offset += packetSize;
-                total_size += packetSize;
+                offset += size;
+                total_size += size;
             }
             System.out.println("Total packet size is " + total_size);
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
+
+
 }
 
